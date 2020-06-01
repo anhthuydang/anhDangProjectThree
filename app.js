12d9daf43291ec86a371ee77405c54749cac94a4
Anh Dang <atd30392@gmail.com>
2020-06-01 18:39:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Cat Adoption Center
+
+// Landing page with the app heading "Cat Adoption Center" and welcome message "Every cat deserves to have a family!"
+
+// A button that allow user to start the process, which will navigate them to a form.
+
+// Listen to the button click event to scroll to form section.
+
+// Prevent default
+
+// A form that prompts user for cat's color and breed with select elements.
+
+// Create an eventlistener on submit button, prevent default behavior
+
+// Get values from user selections
+
+// Create user choice array
+
+// Enter into the Breed type that matches variable
+
+// Loop through breed type and use if statement checking for age/color matched
+
+// When matched is found, push to user choice array
+
+// Get a random cat from the array (or get all the cats matched)
+
+// Display images and information of matched results 
+
+
+	
