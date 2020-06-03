22e2e9dd1ffaddeef89ed5851e224db612d9f226
Anh Dang <atd30392@gmail.com>
2020-06-02 21:20:16 -0400

    add more JS code

--- app.js
+++ app.js
@@ -1,75 +1,129 @@
 // Cat Adoption Center
-const cat = {
-    longHair: [
-        {
-            name: 'Onion',
-            color: 'yellow',
-            url: 'assets/yellowCat.jpg'
-        },
-        {
-            name: 'Poutine',
-            color: 'mix',
-            url: 'assets/mixCat.jpg'
+const adoptionApp = {};
 
-        },
-        {
-            name: 'Sushi',
-            color: 'white',
-            url: 'assets/whiteCat.jpg'
+    adoptionApp.cat = {
+        longHair: [
+            {
+                name: 'Onion',
+                color: 'yellow',
+                breed: 'Domestic longhair',
+                url: 'assets/yellowCat.jpg'
+            },
+            {
+                name: 'Poutine',
+                color: 'mix',
+                breed: 'Domestic longhair',
+                url: 'assets/mixCat.jpg'
 
-        }
-        
-    ],
-    shortHair: [
-        {
-            name: 'Apple',
-            color: 'white'
-        },
-        {
-            name: 'Peanut',
-            color: 'yellow'
-        },
-        {
-            name: 'Cookie',
-            color: 'mix'
-        }
-    ]
-};
+            },
+            {
+                name: 'Sushi',
+                color: 'white',
+                breed: 'Domestic longhair',
+                url: 'assets/whiteCat.jpg'
 
-// Document ready
+            }
+            
+        ],
+        shortHair: [
+            {
+                name: 'Apple',
+                color: 'white',
+                breed: 'Domestic shorthair',
+                url: 'assets/whiteCat2.jpg'
+            },
+            {
+                name: 'Peanut',
+                color: 'yellow',
+                breed: 'Domestic shorthair',
+                url: 'assets/yellow2.jpg'
+            },
+            {
+                name: 'Cookie',
+                color: 'mix',
+                breed: 'Domestic shorthair',
+                url: 'assets/mix2.jpg'
+            }
+        ]
+    };
+    // This is init method
+    adoptionApp.init = () => {
+        $('main form').on('submit', function(e) {
+            e.preventDefault();
+    
+            const userBreed = $('input[name="breed"]:checked').val();
+            const userColor = $('input[name="color"]:checked').val();
+
+            const choices = [];
+
+            const chosenBreed = adoptionApp.cat[userBreed];
+
+            for (let i = 0; i < chosenBreed.length; i++) {
+                const chosenCat = chosenBreed[i];
+                if (chosenCat.color === userColor) {
+                    choices.push(chosenCat);
+                }
+            }
+        //getRandomCat function
+        const getRandomCat = choicesArray => {
+            const randomIndex = Math.floor(Math.random() * choicesArray.length);
+            return choicesArray[randomIndex];
+        } 
+
+        const finalCat = getRandomCat(choices);
+        $('.results').append(`
+            <img src="${finalCat.url}" alt="This is picture of a ${finalCat.color} cat">
+            <h3>Name: ${finalCat.name}</h3>
+            <p>Breed: ${finalCat.breed}</p>
+            <p>Color: ${finalCat.color}</p>
+            <button class="adoptCatButton">Adopt this cat!</button>`);
+        })
+    }
+
 $(function() {
-    console.log("hiiiiii");
-
-
-
-
-
-
+    adoptionApp.init();
 });
-
-// A button that allow user to start the process, which will navigate them to a form.
-
-// Listen to the button click event to scroll to form section.
-
-// Prevent default
-
-// A form that prompts user for cat's color and breed with select elements.
-
-// Create an eventlistener on submit button, prevent default behavior
-
-// Get values from user selections
-
-// Create user choice array
-
-// Enter into the Breed type that matches variable
-
-// Loop through breed type and use if statement checking for age/color matched
-
-// When matched is found, push to user choice array
 
 // Get a random cat from the array (or get all the cats matched)
 
 // Display images and information of matched results 
 
-
 	
+// $(function() {
+// 	$('form').on('submit', function(e) {
+// 		e.preventDefault();
+// // 		// Get values of our selected radio buttons
+// 		const userDrink = $('input[name="beverage"]:checked').val();
+// 		const userPrice = $('input[name="price"]:checked').val();
+		
+// 		// creat an options array
+// 		const options = [];
+// 		// enter into drink type that matches variable
+// 		const drinkSpecs = drinks[userDrink];
+		
+// 		// loop through array  for our drink type variable and ...
+// 		for (let i = 0; i < drinkSpecs.length; i++) {
+// 			const store = drinkSpecs[i];
+// 			// while looping use if statements checking for price matches
+// 			if (store.price === userPrice) {
+// 				// when match is found , push to options arrays
+// 				options.push(store);
+// 			}
+// 		}
+// 		// get a random store from options array
+// 		const finalStore = getRandomItem(options);
+// 		console.log(finalStore);
+// 		// append that store to results section
+// 		$('.results').append(`<h2 class="choice">${finalStore.title}</h2>`)
+// 	});
+
+// });
+
+// const getRandomItem = (optionsArray) => {
+// 	const randomIndex = Math.floor(Math.random() * optionsArray.length);
+// 	return optionsArray[randomIndex];
+// }
+
+
+
+
