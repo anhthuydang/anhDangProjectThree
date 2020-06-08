03541f33cd801e3a8073d2203ba97dc6a78575d4
Anh Dang <atd30392@gmail.com>
2020-06-08 16:17:31 -0400

    completed, rrestructured JS code

--- app.js
+++ app.js
@@ -41,64 +41,78 @@ const adoptionApp = {};
         }
 
     ];
+    
+    //getRandomCat function (in case have more cats available for adoption in future)
+    const getRandomCat = (chosenCatsArray) => {
+        const randomIndex = Math.floor(Math.random() * chosenCatsArray.length);
+        return chosenCatsArray[randomIndex];
+    } 
+
+    // startAdopCat Function
+    const startAdoptCat = () => {
+            // Smooth scroll to form section
+            $("html, body").animate({
+                scrollTop: $("h2").offset().top}, "slow");
+    };
+
+    // fintMyCat function
+    const findMyCat = () => {
+        $('.results').empty();
+        // Get values selected by user and assign them to variables
+        const userBreed = $('input[name="breed"]:checked').val();
+        const userColor = $('input[name="color"]:checked').val();
+        // Filter through array to find matched cat(s)
+        const chosenCats = adoptionApp.cats.filter(function(catObject) {
+            return catObject.breed === userBreed && catObject.color === userColor;
+        })
+        // Get a random (picked) cat and assign to a variable
+        const finalCat = getRandomCat(chosenCats);
+        //  Display image and information of the chosen cat 
+        $('.results').append(`
+        <div class="chosenCatImg">
+            <img src="${finalCat.url}" alt="This is picture of a ${finalCat.color} cat">
+        </div>
+        <div class="chosenCatInfo">
+            <p><span>Name: ${finalCat.name}</span></p>
+            <p>Breed: ${finalCat.breed}</p>
+            <p>Color: ${finalCat.color}</p>
+            <button class="adoptCatButton">Adopt this cat!</button>
+        </div>
+        `);
+
+        // Smooth scroll to result section
+        $("html, body").animate({
+            scrollTop: $(".results").offset().top}, "slow");
+    };
+
+    // displayAdoptForm function
+    const displayAdoptForm = () => {
+        $('.adoptFormSection').addClass('adoptFormSectionDisplay');
+            // smooth scroll
+            $("html, body").animate({
+                scrollTop: $(".adoptFormSection").offset().top}, "slow");
+    };
+
+
     // Init function
     adoptionApp.init = () => {
         // Eventlistener when user clicks "Start Adopting" button
         $('.headerButton').on('click', function(e) {
             e.preventDefault();
-            // Smooth scroll to form section
-            $("html, body").animate({
-                scrollTop: $("h2").offset().top}, "slow");
+            startAdoptCat();
         })
 
         // Eventlistener when user clicks "Find my cat" button
         $('.findCatButton').on('click', function(e) {
             e.preventDefault();
-            $('.results').empty();
-            
-            // Get values selected by user and assign them to variables
-            const userBreed = $('input[name="breed"]:checked').val();
-            const userColor = $('input[name="color"]:checked').val();
-            // Filter through array to find matched cat(s)
-            const chosenCats = adoptionApp.cats.filter(function(catObject) {
-                return catObject.breed === userBreed && catObject.color === userColor;
-            })
-            // Get a random (picked) cat and assign to a variable
-            const finalCat = getRandomCat(chosenCats);
-            //  Display image and information of the chosen cat 
-            $('.results').append(`
-            <div class="chosenCatImg">
-                <img src="${finalCat.url}" alt="This is picture of a ${finalCat.color} cat">
-            </div>
-            <div class="chosenCatInfo">
-                <p><span>Name: ${finalCat.name}</span></p>
-                <p>Breed: ${finalCat.breed}</p>
-                <p>Color: ${finalCat.color}</p>
-                <button class="adoptCatButton">Adopt this cat!</button>
-            </div>
-            `);
-
-            // Smooth scroll to result section
-            $("html, body").animate({
-                scrollTop: $(".results").offset().top}, "slow");
-            
-            
+            findMyCat();
         })
+
         // Eventlistener to make the Adoption Form display and smooth scroll to this section
         $('.results').on('click', function(e) {
             e.preventDefault();
-            $('.adoptFormSection').addClass('adoptFormSectionDisplay');
-            // smooth scroll
-            $("html, body").animate({
-                scrollTop: $(".adoptFormSection").offset().top}, "slow");
+            displayAdoptForm();
         })
-
-        //getRandomCat function (in case have more cats available for adoption in future)
-        const getRandomCat = (chosenCatsArray) => {
-            const randomIndex = Math.floor(Math.random() * chosenCatsArray.length);
-            return chosenCatsArray[randomIndex];
-        } 
-
     }
 
 // document ready function
